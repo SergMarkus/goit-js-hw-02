06a485ee2adbf3c56fd89df5a89fcd0410f4b816
Sergii Morozov <sergmarkus@gmail.com>
2024-07-19 23:00:22 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -3,11 +3,11 @@ function getShippingCost(country) {
     let price;
 
     switch (country) {
+        case "Australia": price = 170;
+            break;
         case "China": price = 100;
             break;
         case "Chile": price = 250;
-            break;
-        case "Australia": price = 170;
             break;
         case "Jamaica": price = 120;
             break;
